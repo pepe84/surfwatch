bc3f5128c75ee1fb4675ba39052c05742dddee5a
Pepe <pepe.84@gmail.com>
2023-11-03 13:51:35 +0100

    Iframes lazy loading, including new spots

--- js/surfwatch.js
+++ js/surfwatch.js
@@ -1,10 +1,21 @@
+var $body = $("body");
+
 // Weather data
 var meteo = {
     "surfwatch-now": "https://portus.puertos.es/index.html#/locationsRTWidget?locationType=Playa&code={code}&theme=dark&locale=es",
     "surfwatch-future": "https://portus.puertos.es/#/locationsPredWidget?locationType=Playa&code={code}&theme=dark&locale=es"
 };
 
-$(document).ready(function () {
+var meteoLoading = false, 
+    meteoReady = false;
+
+function loadMeteo() {
+    // Add preloader
+    var $template = $("<img></img>", {
+        src: "./img/1475_original.gif",
+        class: "surfwatch-loading"
+    });
+    meteoLoading = true;
     // Create iframes
     var $parent = $(".surfwatch-meteo");
     var loaded = 0;
@@ -12,50 +23,62 @@ $(document).ready(function () {
 
     $parent.each(function(){
         console.log("Loading meteo code " + this.id);
+        $template.clone().appendTo(this);
         for (var m in meteo) {
             let url = meteo[m].replace("{code}", this.id);
-            let $iframe = $('<iframe></iframe>', {
+            let $iframe = $("<iframe></iframe>", {
                 src: url,
                 class: m,
                 scrolling: "no",
                 frameborder: "0"
             });
             $iframe.bind("load", function(){
-                $iframe.hide();
                 loaded++;
                 console.log("Frames loaded: " + loaded + " of " + total);
                 if (loaded == total) {
-                    // Enable show/hide buttons
-                    var classes = [
-                        "surfwatch-webcam",
-                        "surfwatch-now",
-                        "surfwatch-future"
-                    ];
-                    for (var i in classes) {
-                        let className = classes[i];
-                        (function(className){
-                            let selector = "." + className;
-                            $("button#"+className).click(function(){
-                                let $btn = $(this);
-                                if ($btn.hasClass("active")) {
-                                    $(selector).hide();
-                                    $btn.removeClass("active");
-                                } else {
-                                    $(selector).show();
-                                    $btn.addClass("active");
-                                }
-                            });
-                        })(className);
-                    }
+                    /**
+                     * Dynamic iframe resize
+                     * @see https://github.com/davidjbradshaw/iframe-resizer/
+                     */
+                    iFrameResize({ log: true }, "iframe");
+                    console.log("iFrame resizing completed")
+                    $(".surfwatch-loading").remove();
+                    meteoLoading = false;
+                    meteoReady = true;
                 }
             });
             $(this).append($iframe);
         }
     });
-});
+}
 
-/**
- * Dynamic iframe resize
- * @see https://github.com/davidjbradshaw/iframe-resizer/
- */
-iFrameResize({ log: true }, 'iframe')
+// Enable show/hide buttons
+var classes = [
+    "surfwatch-webcam",
+    "surfwatch-now",
+    "surfwatch-future"
+];
+
+for (var i in classes) {
+    let className = classes[i];
+    (function(className){
+        let bodyClassName = "has-" + className;
+        $("button#"+className).click(function(){
+            // Change button status
+            if (!meteoLoading) {
+                let $btn = $(this);
+                if ($btn.hasClass("active")) {
+                    $btn.removeClass("active");
+                    $body.removeClass(bodyClassName);
+                } else {
+                    $btn.addClass("active");
+                    $body.addClass(bodyClassName);
+                }
+            }
+            // iFrames lazy loading
+            if (!meteoReady && !meteoLoading) {
+                loadMeteo();
+            }
+        });
+    })(className);
+}
