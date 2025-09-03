d1d237809d2bcec9b46918a3697c796fd25c218b
Pep <jjimen2@xtec.cat>
2025-09-03 17:48:32 +0200

    Move images from public to assets folder

--- src/components/TerritoryTitle.jsx
+++ src/components/TerritoryTitle.jsx
@@ -1,9 +1,18 @@
+const flags = import.meta.glob("../assets/flags/*", {
+  eager: true,
+  import: "default",
+});
+
 function TerritoryTitle({ territory }) {
+  const flagSrc = territory.flag
+    ? flags[`../assets/flags/${territory.flag}`]
+    : null;
+
   return (
     <>
-      {territory.flag && (
+      {flagSrc && (
         <img
-          src={`/flags/${territory.flag}`}
+          src={flagSrc}
           alt={territory.name}
           className="territory-flag me-2"
         />
