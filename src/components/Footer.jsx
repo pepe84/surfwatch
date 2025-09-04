ede1890251e41027e7e1c62b784e03969e24f4cd
Pep <jjimen2@xtec.cat>
2025-09-04 09:45:02 +0200

    Icon author at footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,8 @@
 function Footer() {
   return (
     <footer className="text-center py-3 mt-4 border-top">
-      <p><span className="flip-horizontal">©</span> surfwatch 2025 | <a href="https://github.com/pepe84/surfwatch">GitHub</a> GPL-3.0 | <a target="_blank" href="https://icons8.com/icon/C09XzqIzGLuV/surf-and-palm">Surf And Palm</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a></p>
+      <p className="mb-0"><span className="flip-horizontal">©</span> surfwatch 2025 | <a href="https://github.com/pepe84/surfwatch">GitHub</a> GPL-3.0</p>
+      <p><small>[ <a target="_blank" href="https://icons8.com/icon/C09XzqIzGLuV/surf-and-palm">Surf And Palm</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a> ]</small></p>
     </footer>
   );
 }
